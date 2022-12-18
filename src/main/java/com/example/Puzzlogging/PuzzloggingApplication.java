cbe0cba7c1f144d1871fc7d07d27d306506c566f
pjoon357 <pjoon357@naver.com>
2022-12-19 00:30:29 +0900

    init

---
+++ src/main/java/com/example/Puzzlogging/PuzzloggingApplication.java
@@ -0,0 +1,13 @@
+package com.example.Puzzlogging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PuzzloggingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PuzzloggingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Puzzlogging/PuzzloggingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Puzzlogging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PuzzloggingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
